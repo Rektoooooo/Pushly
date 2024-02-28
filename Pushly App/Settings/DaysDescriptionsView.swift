167791a14b21dfcf849e9b5963f3b5c4d5c81200
Sebastian <sebastian.kucera@icloud.com>
2024-02-28 10:22:29 +0100

    Crash fixes and other stuff

---
+++ Pushly App/Settings/DaysDescriptionsView.swift
@@ -0,0 +1,18 @@
+//
+//  DaysDescriptionsView.swift
+//  Pushly App
+//
+//  Created by Sebastián Kučera on 24.02.2024.
+//
+
+import SwiftUI
+
+struct DaysDescriptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DaysDescriptionsView()
+}
