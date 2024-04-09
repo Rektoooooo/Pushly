29e16bda8e0a18184a560b42ea64c4188ebdb5ae
Sebastian <sebastian.kucera@icloud.com>
2024-04-09 09:53:42 +0200

    Notification (Not working)

---
+++ Pushly App/Ending/ChallangeEndSumaryView.swift
@@ -0,0 +1,18 @@
+//
+//  ChallangeEndSumaryView.swift
+//  Pushly App
+//
+//  Created by Sebastián Kučera on 06.03.2024.
+//
+
+import SwiftUI
+
+struct ChallangeEndSumaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallangeEndSumaryView()
+}
